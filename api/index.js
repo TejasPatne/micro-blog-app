ee4c6e680e00d25a7f9d1977ed967d9287eb4b9b
Tejas Patne <tejaspatne18@gmail.com>
2024-06-02 14:57:03 +0530

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log('API listening on port 3000!'));
