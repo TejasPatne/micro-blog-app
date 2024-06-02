da93bdad0a55f2fbd9b472e50e5be88b8069e687
Tejas Patne <tejaspatne18@gmail.com>
2024-06-02 15:14:19 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
 
 const app = express();
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
