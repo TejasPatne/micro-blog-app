6007c26a5f4872492cc3b205c0b47786da4c7ee9
Tejas Patne <tejaspatne18@gmail.com>
2024-06-02 17:19:54 +0530

    create sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <nav className="bg-neutral-950 text-neutral-400">
       <div className="flex justify-between items-center max-w-6px mx-auto p-3">
-        <NavLink to="/"><h1 className="font-bold"><span className='text-yellow-500'>Maahi</span>ti</h1></NavLink>
+        <NavLink to="/"><h1 className="font-semibold text-xl"><span className='text-yellow-500'>Maahi</span>ti</h1></NavLink>
         <ul className="flex gap-4">
           <NavLink to="/"><li>Home</li></NavLink>
           <NavLink to="/about"><li>About</li></NavLink>
